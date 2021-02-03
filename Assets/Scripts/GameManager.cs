609ab10a57f0a46d54a25efdfce65e3a01bf050a
Pau <pau.gisbert.7e3@itb.cat>
2021-02-03 17:45:57 +0100

    Primer Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    float puntos=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPuntos(float points){
+        puntos=points;
+        Debug.Log(""+points);
+    }
+
+    public float getPuntos(){
+        return puntos;
+    }
+}
---
+++ Assets/Scripts/pulsador.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class pulsador : MonoBehaviour
+{
+    public KeyCode key;
+    bool active = false;
+    GameObject note;
+    static float puntos=0;
+    public Text textoPuntos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(key)&&active){
+            puntos++;
+            textoPuntos.text="Total de puntos: "+puntos;
+            Debug.Log("Total de puntos: "+puntos);
+            Destroy(note);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        active = true;
+        if (col.gameObject.tag=="Note")
+        {
+            note=col.gameObject;
+            
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col){
+        active=false;
+        Destroy(note);
+    }
+}
